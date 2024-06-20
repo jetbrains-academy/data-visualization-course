d54b694adf1d668641ce7bc6701515f6a23c164f
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-06-20 18:12:45 +0200

    fix: remove commented code

--- common/seaborn_test_mixins.py
+++ common/seaborn_test_mixins.py
@@ -36,13 +36,6 @@ class BaseTestMixin(TestCase):
             ),
         )
 
-    # def checkCollectionType(self, fig: FacetGrid, expected_type: Type[plt.Collection], collection_number: int = 0):
-    #     self.assertIsInstance(
-    #         fig.ax.collections[collection_number],
-    #         expected_type,
-    #         f'The collection must have an instance of `{expected_type}`',
-    #     )
-
     @staticmethod
     def checkCollectionPosition(
         fig: FacetGrid,
@@ -91,81 +84,3 @@ class BaseTestMixin(TestCase):
 
     def checkLineColor(self, fig: FacetGrid, expected_color: str, line_number: int = 0):
         self.assertTrue(same_color(to_rgb(expected_color), to_rgb(fig.ax.lines[line_number].get_color())))
-
-
-# class RelplotTestMixin(ABC, TestCase):
-#     @property
-#     @abstractmethod
-#     def kind(self) -> Literal['scatter', 'line']:
-#         raise NotImplementedError
-#
-#     # TODO: make it better
-#     def checkRelplotKind(self, fig: FacetGrid):
-#         ax = fig.ax
-#
-#         collections_exists = getattr(ax, 'collections', None) is not None
-#         lines_exists = getattr(ax, 'lines', None) is not None
-#
-#         if collections_exists and lines_exists:
-#             actual_kind = 'line'
-#         elif collections_exists:
-#             actual_kind = 'scatter'
-#         elif lines_exists:
-#             actual_kind = 'line'
-#         else:
-#             actual_kind = None
-#
-#         self.assertIsNotNone(
-#             actual_kind,
-#             "Can't determine the kind of a figure. Use `seaborn.relational.relplot` for plotting",
-#         )
-#
-#         self.assertEqual(
-#             self.kind,
-#             actual_kind,
-#             f"The figure must be build a {self.kind} plot. Pass `kind='{self.kind}'` to the function",
-#         )
-#
-#     def checkNumberOfTraces(self, fig: FacetGrid, expected_number: int):
-#         if self.kind == 'scatter':
-#             actual_number = len(fig.ax.collections)
-#         else:
-#             actual_number = len(fig.ax.lines)
-#
-#         self.assertEqual(
-#             expected_number,
-#             actual_number,
-#             f'The number of traces on the figure must be exactly {expected_number}.',
-#         )
-#
-#     def checkDataPosition(self, fig: FacetGrid, expected_x: List, expected_y: List):
-#         if self.kind == 'scatter':
-#             actual_x, actual_y = fig.ax.collections[0].get_offsets().T
-#         else:
-#             actual_x, actual_y = fig.ax.lines[0].get_xydata().T
-#
-#         # TODO: error messages
-#         assert_array_almost_equal(actual_x, expected_x, err_msg='x')
-#         assert_array_almost_equal(actual_y, expected_y, err_msg='y')
-#
-#     def checkTraceTransparency(self, fig: FacetGrid, expected_alpha: float):
-#         if self.kind == 'scatter':
-#             actual_alpha = fig.ax.collections[0].get_alpha()
-#         else:
-#             actual_alpha = fig.ax.lines[0].get_alpha()
-#
-#         if actual_alpha is None:
-#             actual_alpha = 1.0
-#
-#         # TODO; error message
-#         assert_allclose(expected_alpha, actual_alpha)
-#
-#     def checkTraceColor(self, fig: FacetGrid, expected_color: str):
-#         expected_rgb = to_rgb(expected_color)
-#
-#         if self.kind == 'scatter':
-#             actual_rgb = to_rgb(fig.ax.collections[0].get_facecolor())
-#         else:
-#             actual_rgb = to_rgb(fig.ax.lines[0].get_color())
-#
-#         self.assertTrue(same_color(expected_rgb, actual_rgb))
