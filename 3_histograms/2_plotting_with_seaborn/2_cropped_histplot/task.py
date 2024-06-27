d4b6a583c7f48b17e7563bc81851cd9ee36b9f04
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-06-27 19:50:20 +0200

    style: fix ruff's issues

--- 3_histograms/2_plotting_with_seaborn/2_cropped_histplot/task.py
+++ 3_histograms/2_plotting_with_seaborn/2_cropped_histplot/task.py
@@ -1,8 +1,8 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 
-from data import read, preprocess
+from data import preprocess, read
 
 
 def plot(games: pd.DataFrame):
