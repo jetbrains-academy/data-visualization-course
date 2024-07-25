75973be0d344ccbfd44802c89c410bc2430967dd
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-07-25 12:19:15 +0400

    fix: renamed tests

--- 1_1_line_and_scatter_plots_seaborn/1_theory/2_transparency_and_color/tests/test_task.py
+++ 1_1_line_and_scatter_plots_seaborn/1_theory/2_transparency_and_color/tests/test_task.py
@@ -20,21 +20,21 @@ class PlotTestCase(BaseTestMixin):
         cls.data = data
         cls.fig = plot(data)
 
-    def test_1_return_type(self):
+    def test_1_1_return_type(self):
         self.checkReturnType(self.fig, expected_type=sns.FacetGrid, expected_function="sns.relplot")
 
-    def test_2_number_of_axes(self):
+    def test_1_2_number_of_axes(self):
         self.checkNumberOfAxes(self.fig.axes.flat, 1)
 
-    def test_3_relplot_kind(self):
+    def test_1_3_relplot_kind(self):
         self.checkNumberOfCollections(self.fig.ax, 1)
         self.checkNumberOfLines(self.fig.ax, 0)
 
-    def test_4_data_position(self):
+    def test_2_1_scatter_position(self):
         self.checkCollectionPosition(self.fig.ax, self.data["user_score"], self.data["critic_score"])
 
-    def test_5_transparency(self):
+    def test_2_2_scatter_transparency(self):
         self.checkCollectionTransparency(self.fig.ax, 0.1)
 
-    def test_6_color(self):
+    def test_2_3_scatter_color(self):
         self.checkCollectionColor(self.fig.ax, "green")
--- 1_1_line_and_scatter_plots_seaborn/1_theory/3_relplot_line/tests/test_task.py
+++ 1_1_line_and_scatter_plots_seaborn/1_theory/3_relplot_line/tests/test_task.py
@@ -20,22 +20,22 @@ class PlotTestCase(BaseTestMixin):
         cls.data = data
         cls.fig = plot(data)
 
-    def test_1_return_type(self):
+    def test_1_1_return_type(self):
         self.checkReturnType(self.fig, expected_type=sns.FacetGrid, expected_function="sns.relplot")
 
-    def test_2_number_of_axes(self):
+    def test_1_2_number_of_axes(self):
         self.checkNumberOfAxes(self.fig.axes.flat, 1)
 
-    def test_3_relplot_kind(self):
+    def test_1_3_relplot_kind(self):
         self.checkNumberOfCollections(self.fig.ax, 1)
         self.checkNumberOfLines(self.fig.ax, 1)
 
-    def test_4_data_position(self):
+    def test_2_1_line_position(self):
         position = self.data.groupby("user_score")["critic_score"].mean()
         self.checkLinePosition(self.fig.ax, position.index, position)
 
-    def test_5_transparency(self):
+    def test_2_2_line_transparency(self):
         self.checkLineTransparency(self.fig.ax, 1)
 
-    def test_6_color(self):
+    def test_2_3_line_color(self):
         self.checkLineColor(self.fig.ax, "C0")
