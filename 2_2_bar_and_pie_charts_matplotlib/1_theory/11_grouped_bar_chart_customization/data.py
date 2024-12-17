eb286edc8762c581bbd19c0a1c59f206e53c8086
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-12-17 18:15:08 +0100

    refactor: rename `get_decades` function for consistency

--- 2_2_bar_and_pie_charts_matplotlib/1_theory/11_grouped_bar_chart_customization/data.py
+++ 2_2_bar_and_pie_charts_matplotlib/1_theory/11_grouped_bar_chart_customization/data.py
@@ -34,7 +34,7 @@ def get_all_regions(data: pd.DataFrame) -> Set[str]:
     return set(data["region"].unique())
 
 
-def get_decades(data: pd.DataFrame) -> Set[int]:
+def get_all_decades(data: pd.DataFrame) -> Set[int]:
     return set(data["decade"].unique())
 
 
