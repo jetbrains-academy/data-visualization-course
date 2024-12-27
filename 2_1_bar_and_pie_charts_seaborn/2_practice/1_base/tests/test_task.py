5d9a513b239e583800e026e7fa6c2a8fc962c8a1
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2024-12-27 16:43:48 +0100

    feat: add a task draft

---
+++ 2_1_bar_and_pie_charts_seaborn/2_practice/1_base/tests/test_task.py
@@ -0,0 +1,58 @@
+from typing import ClassVar
+
+from matplotlib.container import BarContainer
+import pandas as pd
+import seaborn as sns
+
+from common.base_test_mixins import BaseTestMixin
+from data import read, get_product_order
+from task import plot
+
+
+class PlotTestCase(BaseTestMixin):
+    data: ClassVar[pd.DataFrame]
+    fig: ClassVar[sns.FacetGrid]
+
+    @classmethod
+    def setUpClass(cls):
+        data = read()
+
+        cls.data = data
+        cls.fig = plot(data)
+
+    def test_1_1_return_type(self):
+        self.checkReturnType(self.fig, expected_type=sns.FacetGrid, expected_function="sns.catplot")
+
+    def test_1_2_number_of_axes(self):
+        self.checkNumberOfAxes(self.fig.axes.flat, 1)
+
+    def test_1_3_catplot_kind(self):
+        self.checkNumberOfCollections(self.fig.ax, 0)
+        self.checkNumberOfLines(self.fig.ax, 0)
+
+        # Bars
+        self.checkNumberOfContainers(self.fig.ax, self.data["category"].nunique())
+        for i, category in enumerate(self.data["category"].unique()):
+            self.checkContainerType(self.fig.ax, BarContainer, container_number=i)
+
+            self.checkNumberOfBars(
+                self.fig.ax,
+                self.data[self.data["category"] == category]["product"].nunique(),
+                container_number=i,
+            )
+
+    def test_2_1_bar_position(self):
+        for i, category in enumerate(self.data["category"].unique()):
+            expected_values = self.data[self.data['category'] == category].groupby('product', sort=False).size()
+            self.checkBarValues(self.fig.ax, expected_values.to_list(), container_number=i)
+
+    def test_2_2_bar_layout(self):
+        for i in range(self.data["category"].nunique()):
+            self.checkBarLayout(self.fig.ax, expected_layout="horizontal", container_number=i)
+
+    def test_2_3_bar_labels(self):
+        self.checkTickLabels(
+            self.fig.ax,
+            get_product_order(self.data),
+            axis="y",
+        )
