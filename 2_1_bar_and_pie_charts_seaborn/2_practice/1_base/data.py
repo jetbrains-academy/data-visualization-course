e4cbaf966963bf234b17ca2641b5c91e2a1bc257
Nikita.Magomedeminov <nikiniks2005@gmail.com>
2025-07-29 10:19:21 +0300

    fix: remove pandas warning

--- 2_1_bar_and_pie_charts_seaborn/2_practice/1_base/data.py
+++ 2_1_bar_and_pie_charts_seaborn/2_practice/1_base/data.py
@@ -11,6 +11,6 @@ def get_product_order(data: pd.DataFrame) -> list:
     return (
         data.drop_duplicates(subset=["category", "product"])
         .groupby("category", sort=False)
-        .apply(lambda group: group["product"].sort_values())
+        .apply(lambda group: group["product"].sort_values(), include_groups=False)
         .to_list()
     )
